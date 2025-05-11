ee7780801e1a1b29d4edb4da3f50b7363386a6bf
Haarsh Wintel <harshkatare977@gmail.com>
2025-05-11 11:10:49 +0530

    variables

---
+++ 01_Variables/variables.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // var.. declaration and initialization
+
+    int a = 30;
+    int b = 10;
+
+    // var.. reassignment
+
+    a = a + 2;
+
+    // using vars in expressions
+
+    int sum = a + b;
+    int product = a * b;
+
+        // outputs
+
+    cout << "Value of a: " << a << endl;
+    cout << "Value of b: " << b << endl;
+    cout << "Sum: " << sum << endl;
+    cout << "Product: " << product << endl;
+
+    return 0;
+}
